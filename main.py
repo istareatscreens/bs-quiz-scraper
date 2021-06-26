e22c4ffa0587da8fb6255103082a375f5c4be4b3
istareatscreens <schar138@uottawa.ca>
2021-06-26 18:57:36 -0400

    draw window and add title

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+window = Tk()  # instantiate window
+# window styling
+window.geometry("600x600")
+window.title("bs-quiz-scraper")
+
+
+window.mainloop()  # render window
