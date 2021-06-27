6b56a23465ba2a21522e7f2814bc2a306a4f5adc
istareatscreens <schar138@uottawa.ca>
2021-06-27 03:17:24 -0400

    Add CustomGridWidget class to remove repeat logic

---
+++ CustomGridWidget.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Button, Label, Entry, Frame, filedialog
+from tkinter.constants import SUNKEN
+
+
+class CustomGridWidget:
+    def __init__(self, window) -> None:
+        # Create Frame
+        self._row = -1
+        self._frame = Frame(window, bd=1, relief=SUNKEN)
+
+    def _incrementAndGetRow(self):
+        self._row += 1
+        return self._row
